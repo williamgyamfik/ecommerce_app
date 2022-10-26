c8852004fb65e79de1a21f5b85c3741dd34a2391
williamgyamfik <chester3gh@gmail.com>
2022-10-26 03:21:36 +0300

    modified Carousel component

---
+++ src/components/Carousel.jsx
@@ -0,0 +1,45 @@
+import slide1 from "../images//noBackgroundImages/slide1.png";
+import slide2 from "../images//noBackgroundImages/slide2.png";
+import slide3 from "../images/noBackgroundImages/slide3.png";
+
+const Carousel = () => {
+  return (
+    <div
+      id="carouselExampleSlidesOnly"
+      className="carousel slide "
+      data-bs-ride="carousel"
+    >
+      <div className="carousel-inner">
+        <div className="carousel-item active " data-bs-interval="5000">
+          <img
+            src={slide1}
+            className="d-block"
+            alt="..."
+            width="150px"
+            height="180px"
+          />
+        </div>
+        <div className="carousel-item" data-bs-interval="7000">
+          <img
+            src={slide2}
+            className="d-block "
+            alt="..."
+            width="150px"
+            height="180px"
+          />
+        </div>
+        <div className="carousel-item" data-bs-interval="10000">
+          <img
+            src={slide3}
+            className="d-block "
+            alt="..."
+            width="150px"
+            height="180px"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Carousel;
